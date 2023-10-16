7e30bb006d0c4c089057f1ff345e3080a522ca09
chenhesen <hesen.chs@alibaba-inc.com>
2023-10-16 10:53:28 +0800

    Modify the location of RayExecutor import (#35)
    - avoid the ray bug

--- data_juicer/core/__init__.py
+++ data_juicer/core/__init__.py
@@ -1,6 +1,5 @@
 from .analyser import Analyser
 from .data import NestedDataset
 from .executor import Executor
-from .ray_executor import RayExecutor
 from .exporter import Exporter
 from .tracer import Tracer
