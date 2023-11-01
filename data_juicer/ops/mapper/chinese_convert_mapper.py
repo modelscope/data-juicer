27687194d54c4f91cabf9fab36ba686ab50a3f23
chenhesen <hesen.chs@alibaba-inc.com>
2023-11-01 10:44:36 +0800

    Feat/add chinese mapper (#51)
    * Modify the location of RayExecutor import
    
    * add two Chinese mapper
    
    * Add op description to docs
    
    * fix md
    
    * keep alphabet number punc
    
    * add op info in overview_scan

---
+++ data_juicer/ops/mapper/chinese_convert_mapper.py
@@ -0,0 +1,47 @@
+import opencc
+
+from ..base_op import OPERATORS, Mapper
+
+
+@OPERATORS.register_module('chinese_convert_mapper')
+class ChineseConvertMapper(Mapper):
+    """Mapper to convert Chinese between Traditional Chinese, Simplified Chinese
+    and Japanese Kanji."""
+
+    def __init__(self, mode: str = 's2t', *args, **kwargs):
+        """
+        Initialization method.
+
+        :param mode: Choose the mode to convert Chinese,
+        s2t: Simplified Chinese to Traditional Chinese,
+        t2s: Traditional Chinese to Simplified Chinese,
+        s2tw: Simplified Chinese to Traditional Chinese (Taiwan Standard),
+        tw2s: Traditional Chinese (Taiwan Standard) to Simplified Chinese,
+        s2hk: Simplified Chinese to Traditional Chinese (Hong Kong variant),
+        hk2s: Traditional Chinese (Hong Kong variant) to Simplified Chinese,
+        s2twp: Simplified Chinese to Traditional Chinese (Taiwan Standard)
+               with Taiwanese idiom,
+        tw2sp: Traditional Chinese (Taiwan Standard) to Simplified Chinese
+               with Mainland Chinese idiom,
+        t2tw: Traditional Chinese to Traditional Chinese (Taiwan Standard),
+        tw2t: Traditional Chinese (Taiwan standard) to Traditional Chinese,
+        hk2t: Traditional Chinese (Hong Kong variant) to Traditional Chinese,
+        t2hk: Traditional Chinese to Traditional Chinese (Hong Kong variant),
+        t2jp: Traditional Chinese Characters (Kyūjitai) to New Japanese Kanji,
+        jp2t: New Japanese Kanji (Shinjitai) to Traditional Chinese Characters,
+        :param args: extra args
+        :param kwargs: extra args
+        """
+        super().__init__(*args, **kwargs)
+        mode_list = [
+            's2t', 't2s', 's2tw', 'tw2s', 's2hk', 'hk2s', 's2twp', 'tw2sp',
+            't2tw', 'tw2t', 'hk2t', 't2hk', 't2jp', 'jp2t'
+        ]
+        assert mode in mode_list, 'Please make sure mode is one of {}'.format(
+            mode_list)
+        self.converter = opencc.OpenCC(mode + '.json')
+
+    def process(self, sample):
+
+        sample[self.text_key] = self.converter.convert(sample[self.text_key])
+        return sample
---
+++ data_juicer/ops/mapper/remove_non_chinese_character_mapper.py
@@ -0,0 +1,45 @@
+import regex as re
+
+from ..base_op import OPERATORS, Mapper
+
+
+@OPERATORS.register_module('remove_non_chinese_character_mapper')
+class RemoveNonChineseCharacterlMapper(Mapper):
+    """Mapper to remove non chinese Character in text samples."""
+
+    def __init__(self,
+                 keep_alphabet: bool = True,
+                 keep_number: bool = True,
+                 keep_punc: bool = True,
+                 *args,
+                 **kwargs):
+        """
+        Initialization method.
+
+        :param keep_alphabet: whether to keep alpabet
+        :param keep_number: whether to keep number
+        :param keep_punc: whether to keep punctuation
+        :param args: extra args
+        :param kwargs: extra args
+        """
+        super().__init__(*args, **kwargs)
+        self.pattern = u'[^\u4e00-\u9fa5'
+        if keep_alphabet:
+            self.pattern += u'A-Za-z'
+        if keep_number:
+            self.pattern += u'0-9'
+        if keep_punc:
+            self.pattern += u'.， ,\\-。%《*》/•、&＆(—)（+）：？!！“”·]+'
+        else:
+            self.pattern += u']'
+
+    def process(self, sample):
+
+        if not re.search(self.pattern, sample[self.text_key], flags=re.DOTALL):
+            return sample
+
+        sample[self.text_key] = re.sub(pattern=self.pattern,
+                                       repl=r'',
+                                       string=sample[self.text_key],
+                                       flags=re.DOTALL)
+        return sample
