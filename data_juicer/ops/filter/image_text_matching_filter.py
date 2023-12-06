2859ea18de528035cfd226c5411790e6b414e975
zhijianma <zhijian.mzj@alibaba-inc.com>
2023-12-06 10:43:01 +0800

    Feature/image text matching (#100)
    * feature: 
    1. add image_text_matching_filter
    2. rename clip_similarity_filter to image_text_similarity_filter
    3 fix get_model in ray executor

---
+++ data_juicer/ops/filter/image_text_matching_filter.py
@@ -0,0 +1,167 @@
+import numpy as np
+from jsonargparse.typing import ClosedUnitInterval
+from PIL import ImageOps
+
+from data_juicer.utils.availability_utils import AvailabilityChecking
+from data_juicer.utils.constant import Fields, StatsKeys
+from data_juicer.utils.mm_utils import (SpecialTokens, load_image,
+                                        remove_special_tokens)
+from data_juicer.utils.model_utils import get_model, prepare_model
+
+from ..base_op import OPERATORS, Filter
+from ..op_fusion import LOADED_IMAGES
+
+OP_NAME = 'image_text_matching_filter'
+
+with AvailabilityChecking(['torch', 'transformers'], OP_NAME):
+    import torch
+    import transformers  # noqa: F401
+
+    # avoid hanging when calling blip in multiprocessing
+    torch.set_num_threads(1)
+
+
+@OPERATORS.register_module(OP_NAME)
+@LOADED_IMAGES.register_module(OP_NAME)
+class ImageTextMatchingFilter(Filter):
+    """Filter to keep samples those matching score between image and text
+    within a specific range."""
+
+    def __init__(self,
+                 hf_blip='Salesforce/blip-itm-base-coco',
+                 min_score: ClosedUnitInterval = 0.003,
+                 max_score: ClosedUnitInterval = 1.0,
+                 horizontal_flip: bool = False,
+                 vertical_flip: bool = False,
+                 any_or_all: str = 'any',
+                 reduce_mode: str = 'avg',
+                 *args,
+                 **kwargs):
+        """
+        Initialization method.
+
+        :param hf_blip: blip model name on huggingface to compute
+            the matching score between image and text.
+        :param min_score: The min matching score to keep samples.
+        :param max_score: The max matching score to keep samples.
+        :param horizontal_flip: Flip image horizontally (left to right).
+        :param vertical_flip: Flip image vertically (top to bottom).
+        :param any_or_all: keep this sample with 'any' or 'all' strategy of
+            all images. 'any': keep this sample if any images meet the
+            condition. 'all': keep this sample only if all images meet the
+            condition.
+        :param reduce_mode: reduce mode when one text corresponds to
+            multiple images in a chunk.
+            'avg': Take the average of multiple values
+            'max': Take the max of multiple values
+            'min': Take the min of multiple values
+        :param args: extra args
+        :param kwargs: extra args
+        """
+        super().__init__(*args, **kwargs)
+        self.min_score = min_score
+        self.max_score = max_score
+        if reduce_mode not in ['avg', 'max', 'min']:
+            raise ValueError(f'Reduce mode [{reduce_mode}] is not supported. '
+                             f'Can only be one of ["avg", "max", "min"].')
+        if any_or_all not in ['any', 'all']:
+            raise ValueError(f'Keep strategy [{any_or_all}] is not supported. '
+                             f'Can only be one of ["any", "all"].')
+        self.any = (any_or_all == 'any')
+        self.model_key = prepare_model(model_type='hf_blip', model_key=hf_blip)
+        self.reduce_mode = reduce_mode
+        self.horizontal_flip = horizontal_flip
+        self.vertical_flip = vertical_flip
+
+    def compute_stats(self, sample, context=False):
+        # check if it's computed already
+        if StatsKeys.image_text_matching_score in sample[Fields.stats]:
+            return sample
+
+        # there is no image in this sample
+        if self.image_key not in sample or not sample[self.image_key]:
+            sample[Fields.stats][
+                StatsKeys.image_text_matching_score] = np.array(
+                    [], dtype=np.float64)
+            return sample
+
+        # load images
+        loaded_image_keys = sample[self.image_key]
+        images = {}
+        for loaded_image_key in loaded_image_keys:
+            if context and loaded_image_key in sample[Fields.context]:
+                # load from context
+                images[loaded_image_key] = sample[
+                    Fields.context][loaded_image_key]
+            else:
+                if loaded_image_key not in images:
+                    # avoid load the same images
+                    image = load_image(loaded_image_key)
+                    images[loaded_image_key] = image
+                    if context:
+                        # store the image data into context
+                        sample[Fields.context][loaded_image_key] = image
+
+        text = sample[self.text_key]
+        offset = 0
+        matching_scores = []
+        model, processor = get_model(self.model_key)
+
+        for chunk in text.split(SpecialTokens.eoc):
+            count = chunk.count(SpecialTokens.image)
+
+            # no image or no text
+            if count == 0 or len(chunk) == 0:
+                continue
+            else:
+                text_chunk = remove_special_tokens(chunk)
+                image_chunk = []
+                for image_key in loaded_image_keys[offset:offset + count]:
+                    image = images[image_key]
+                    if self.horizontal_flip:
+                        image = ImageOps.mirror(image)
+                    if self.vertical_flip:
+                        image = ImageOps.flip(image)
+                    image_chunk.append(image)
+
+                inputs = processor(text=text_chunk,
+                                   images=image_chunk,
+                                   return_tensors='pt',
+                                   truncation=True,
+                                   max_length=model.config.text_config.
+                                   max_position_embeddings,
+                                   padding=True)
+
+                outputs = model(**inputs)
+                itm_scores = outputs.itm_score.detach().cpu().softmax(
+                    dim=-1)[:, 1]
+
+                if self.reduce_mode == 'avg':
+                    chunk_itm_score = itm_scores.mean()
+                elif self.reduce_mode == 'max':
+                    chunk_itm_score = itm_scores.max()
+                else:
+                    chunk_itm_score = itm_scores.min()
+
+                matching_scores.append(float(chunk_itm_score))
+            offset += count
+        sample[Fields.stats][
+            StatsKeys.image_text_matching_score] = matching_scores
+
+        return sample
+
+    def process(self, sample):
+        itm_scores = sample[Fields.stats][StatsKeys.image_text_matching_score]
+        if len(itm_scores) <= 0:
+            return True
+
+        keep_bools = np.array([
+            self.min_score <= itm_score <= self.max_score
+            for itm_score in itm_scores
+        ])
+
+        # different strategies
+        if self.any:
+            return keep_bools.any()
+        else:
+            return keep_bools.all()
--- tests/ops/filter/test_image_text_similarity_filter.py
+++ tests/ops/filter/test_image_text_similarity_filter.py
@@ -3,12 +3,12 @@ import unittest
 
 from datasets import Dataset
 
-from data_juicer.ops.filter.clip_similarity_filter import ClipSimilarityFilter
+from data_juicer.ops.filter.image_text_similarity_filter import ImageTextSimilarityFilter
 from data_juicer.utils.constant import Fields
 from data_juicer.utils.mm_utils import SpecialTokens
 
 
-class ClipSimilarityFilterTest(unittest.TestCase):
+class ImageTextSimilarityFilterTest(unittest.TestCase):
 
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..',
                              'data')
@@ -47,14 +47,42 @@ class ClipSimilarityFilterTest(unittest.TestCase):
         }]
 
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='avg',
                                   any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op)
 
     def test_eoc_special_token(self):
+
+        ds_list = [{
+            'text':
+            f'{SpecialTokens.image}a photo of a cat{SpecialTokens.eoc}',
+            'images': [self.cat_path]
+        }, {
+            'text': f'{SpecialTokens.image}a photo of a dog{SpecialTokens.eoc}',
+            'images': [self.cat_path]
+        }]
+        tgt_list = [{
+            'text':
+            f'{SpecialTokens.image}a photo of a cat{SpecialTokens.eoc}',
+            'images': [self.cat_path]
+        }]
+
+        dataset = Dataset.from_list(ds_list)
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
+                                  reduce_mode='avg',
+                                  any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
+                                  min_score=0.2,
+                                  max_score=0.9)
+        self._run_filter(dataset, tgt_list, op)
+
+    def test_horizontal_flip(self):
 
         ds_list = [{
             'text':
@@ -71,9 +99,37 @@ class ClipSimilarityFilterTest(unittest.TestCase):
         }]
 
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='avg',
                                   any_or_all='any',
+                                  horizontal_flip=True,
+                                  vertical_flip=False,
+                                  min_score=0.24,
+                                  max_score=0.9)
+        self._run_filter(dataset, tgt_list, op)
+
+    def test_vertical_flip(self):
+
+        ds_list = [{
+            'text':
+            f'{SpecialTokens.image}a photo of a cat{SpecialTokens.eoc}',
+            'images': [self.cat_path]
+        }, {
+            'text': f'{SpecialTokens.image}a photo of a dog',
+            'images': [self.cat_path]
+        }]
+        tgt_list = [{
+            'text':
+            f'{SpecialTokens.image}a photo of a cat{SpecialTokens.eoc}',
+            'images': [self.cat_path]
+        }]
+
+        dataset = Dataset.from_list(ds_list)
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
+                                  reduce_mode='avg',
+                                  any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=True,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op)
@@ -93,9 +149,11 @@ class ClipSimilarityFilterTest(unittest.TestCase):
             'images': [self.cat_path, self.cat_path]
         }]
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='avg',
                                   any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op)
@@ -110,9 +168,11 @@ class ClipSimilarityFilterTest(unittest.TestCase):
         }]
         tgt_list = []
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='avg',
                                   any_or_all='all',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op)
@@ -130,9 +190,11 @@ class ClipSimilarityFilterTest(unittest.TestCase):
             'images': [self.cat_path, self.img3_path]
         }]
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='avg',
                                   any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op)
@@ -150,9 +212,11 @@ class ClipSimilarityFilterTest(unittest.TestCase):
             'images': [self.cat_path, self.img3_path]
         }]
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='max',
                                   any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op)
@@ -171,9 +235,11 @@ class ClipSimilarityFilterTest(unittest.TestCase):
         }]
 
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='min',
                                   any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.1,
                                   max_score=0.9)
 
@@ -197,9 +263,11 @@ class ClipSimilarityFilterTest(unittest.TestCase):
             'images': [self.cat_path, self.cat_path]
         }] * 10
         dataset = Dataset.from_list(ds_list)
-        op = ClipSimilarityFilter(hf_clip=self.hf_clip,
+        op = ImageTextSimilarityFilter(hf_clip=self.hf_clip,
                                   reduce_mode='avg',
                                   any_or_all='any',
+                                  horizontal_flip=False,
+                                  vertical_flip=False,
                                   min_score=0.2,
                                   max_score=0.9)
         self._run_filter(dataset, tgt_list, op, num_proc=4)
