328f52878687128b0bc3e0c5870c85e1684f6579
lielin.hyl <lielin.hyl@alibaba-inc.com>
2023-12-15 18:06:46 +0800

    * output the cleaning log when the cleaning actually happens

--- data_juicer/utils/unittest_utils.py
+++ data_juicer/utils/unittest_utils.py
@@ -10,15 +10,15 @@ class DataJuicerTestCaseBase(unittest.TestCase):
         import transformers
         if hf_model_name:
             # given the hf model name, remove this model only
-            print(f'CLEAN model cache files for {hf_model_name}')
             model_dir = os.path.join(
                 transformers.TRANSFORMERS_CACHE,
                 f'models--{hf_model_name.replace("/", "--")}'
             )
             if os.path.exists(model_dir):
+                print(f'CLEAN model cache files for {hf_model_name}')
                 shutil.rmtree(model_dir)
         else:
             # not giben the hf model name, remove the whole TRANSFORMERS_CACHE
-            print('CLEAN all TRANSFORMERS_CACHE')
             if os.path.exists(transformers.TRANSFORMERS_CACHE):
+                print('CLEAN all TRANSFORMERS_CACHE')
                 shutil.rmtree(transformers.TRANSFORMERS_CACHE)
