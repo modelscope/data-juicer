ab77b04c7b3da494afa258bfb1c68dee4c3c2cec
zhijian.mzj <zhijian.mzj@alibaba-inc.com>
2024-03-13 18:38:05 +0800

    add converting tools between msr-vtt and dj

--- tools/multimodal/source_format_to_data_juicer_format/internvid_to_dj.py
+++ tools/multimodal/source_format_to_data_juicer_format/internvid_to_dj.py
@@ -52,7 +52,7 @@ def main(
     eoc_special_token: str = SpecialTokens.eoc,
     video_special_token: str = SpecialTokens.video,
     add_eoc_at_last: bool = True,
-    sent_seperator: str = ' ',
+    sent_separator: str = ' ',
     video_special_token_insert_pos: str = 'before',
     cut_videos: bool = True,
     cut_video_store_path: str = None,
@@ -74,7 +74,7 @@ def main(
         Data-Juicer).
     :param add_eoc_at_last: whether to add an extra eoc_special_token at the
         end of text. Default: False.
-    :param sent_seperator: seperator to split different sentences or tokens.
+    :param sent_separator: separator to split different sentences or tokens.
         Default: " "
     :param video_special_token_insert_pos: the position in the sentence to
         insert the corresponding video special token. Should be one of: [
@@ -118,7 +118,7 @@ def main(
                 # add video special token
                 new_sample, text = convert_text_to_dj(
                     text, s, add_eoc_at_last, eoc_special_token,
-                    keep_other_fields, sent_seperator, video_special_token,
+                    keep_other_fields, sent_separator, video_special_token,
                     video_special_token_insert_pos)
 
                 # cut videos if needed
