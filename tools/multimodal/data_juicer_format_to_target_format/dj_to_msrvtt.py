172e5d65551c68f4f8b56659b56dbb7c3b5fea55
BeachWang <1400012807@pku.edu.cn>
2024-04-11 19:59:13 +0800

    convert absolute path to relative path for multi sources (#288)
    * absolute path to relative path
    
    * pre-commit done
    
    * fix doc
    
    * json to josnl
    
    * pre-commit done
    
    * move copy_data

--- tools/multimodal/data_juicer_format_to_target_format/dj_to_msrvtt.py
+++ tools/multimodal/data_juicer_format_to_target_format/dj_to_msrvtt.py
@@ -53,8 +53,6 @@ def main(
     eoc_special_token: str = SpecialTokens.eoc,
     video_special_token: str = SpecialTokens.video,
     sent_separator: str = ' ',
-    convert_to_relative_paths: bool = False,
-    original_msr_vtt_ds_path: str = None,
 ):
     """
     Convert a Data-Juicer-format dataset to a MSR-VTT-like dataset.
@@ -71,14 +69,6 @@ def main(
         special token from our Data-Juicer. Default: <__dj__video> (from
         Data-Juicer).
     :param sent_separator: separator to split different sentences. Default: " "
-    :param convert_to_relative_paths: whether convert the video paths in this
-        dataset to relative paths to the original dataset. If it's True, an
-        extra argument original_msr_vtt_ds_path is required. When the
-        processed and converted dataset will be used in another machine, it's
-        better to set this argument to True. Default: False.
-    :param original_msr_vtt_ds_path: path to the original unprocessed
-        MSR-VTT dataset, which is used to help to recover the relative video
-        paths for better migration. Default: None.
         """
     # ----- Constant settings. Better not to change them. -----
     text_key = 'text'  # default key of field to store the sample text
@@ -101,20 +91,6 @@ def main(
             f'Create directory [{os.path.dirname(target_msr_vtt_ds_path)}] '
             f'for the target dataset.')
         os.makedirs(os.path.dirname(target_msr_vtt_ds_path))
-    # if convert_to_relative_paths is True, check if the
-    # original_msr_vtt_ds_path is provided as well.
-    if convert_to_relative_paths:
-        if not original_msr_vtt_ds_path:
-            raise ValueError('When convert_to_relative_paths is set to True, '
-                             'the original_msr_vtt_ds_path must be provided '
-                             'for recovering the relative paths. Please '
-                             'check and retry.')
-        original_msr_vtt_ds_path = os.path.abspath(original_msr_vtt_ds_path)
-        # if provided original_msr_vtt_ds_path is the dataset file path, only
-        # keep the directory path.
-        if os.path.isfile(original_msr_vtt_ds_path):
-            original_msr_vtt_ds_path = os.path.dirname(
-                original_msr_vtt_ds_path)
 
     # save MSR-VTT dataset from Data-Juicer format
     logger.info('Start converting the original dataset to MSR-VTT format...')
@@ -130,20 +106,6 @@ def main(
                     new_sample[key] = s[key]
 
                 # add video
-                if convert_to_relative_paths:
-                    if video.startswith(original_msr_vtt_ds_path):
-                        video = os.path.relpath(video,
-                                                original_msr_vtt_ds_path)
-                    else:
-                        raise ValueError(
-                            f'The original_msr_vtt_ds_path '
-                            f'[{original_msr_vtt_ds_path}] is not the '
-                            f'directory that contains the video '
-                            f'[{video}] in the sample of line number '
-                            f'[{line_num}]. Please check if the correct '
-                            f'original_msr_vtt_ds_path is provided or '
-                            f'something wrong with this sample, and try '
-                            f'again later.')
                 new_sample[tgt_video_key] = video
 
                 # add caption
