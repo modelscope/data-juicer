4c4cb3023407d5c394195fb790aefbae50cb4129
hesen.chs <hesen.chs@alibaba-inc.com>
2024-05-20 15:30:41 +0800

    del print

--- tests/ops/mapper/test_video_split_by_key_frame_mapper.py
+++ tests/ops/mapper/test_video_split_by_key_frame_mapper.py
@@ -65,7 +65,6 @@ class VideoSplitByKeyFrameMapperTest(DataJuicerTestCaseBase):
         dataset = NestedDataset.from_list(source_list)
         dataset = dataset.map(op.process, num_proc=num_proc)
         res_list = self._get_res_list(dataset, source_list)
-        print(dataset, res_list)
         self.assertEqual(res_list, target_list)
 
     def test(self):
