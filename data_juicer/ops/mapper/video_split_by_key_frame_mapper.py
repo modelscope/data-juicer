9b76fd374ca0631ad29952bc2f908861a2c90940
hesen.chs <hesen.chs@alibaba-inc.com>
2024-05-20 14:55:12 +0800

    pre commit

--- data_juicer/ops/mapper/video_split_by_key_frame_mapper.py
+++ data_juicer/ops/mapper/video_split_by_key_frame_mapper.py
@@ -74,7 +74,7 @@ class VideoSplitByKeyFrameMapper(Mapper):
 
         if Fields.source_file not in sample or not sample[Fields.source_file]:
             sample[Fields.source_file] = sample[self.video_key]
-            
+
         # the split results
         split_sample = copy.deepcopy(sample)
         split_sample[self.text_key] = ''
