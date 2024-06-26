c749a2837b4591f5b1042deb80aa32c7b19ecd97
Xuchen Pan <32844285+pan-x-c@users.noreply.github.com>
2024-06-26 15:51:56 +0800

    Add more unittest  (#304)
    * add unittest env with gpu
    
    * fix unittest yml
    
    * add environment for unittest
    
    * update workflow trigger
    
    * update install step
    
    * fix install command
    
    * update working dir
    
    * update container
    
    * update working dir
    
    * change working directory
    
    * change working directory
    
    * change working directory
    
    * change working directory
    
    * change unittest
    
    * use test tag
    
    * finish tag support
    
    * support run op with different executro
    
    * fix pre-commit
    
    * add hf mirror
    
    * add hf mirror
    
    * run all test in standalone mode by default
    
    * ignore image face ratio
    
    * update tags
    
    * add ray testcase
    
    * add ray test in workflow
    
    * update ray unittest workflow
    
    * delete old unittest
    
    ---------
    
    Co-authored-by: root <panxuchen>

--- data_juicer/utils/unittest_utils.py
+++ data_juicer/utils/unittest_utils.py
@@ -2,9 +2,28 @@ import os
 import shutil
 import unittest
 
+import numpy
+import pyarrow as pa
+import ray.data as rd
+from datasets import Dataset
+
+from data_juicer.ops import Filter
+from data_juicer.utils.constant import Fields
 from data_juicer.utils.registry import Registry
 
 SKIPPED_TESTS = Registry('SkippedTests')
+
+
+def TEST_TAG(*tags):
+    """Tags for test case.
+    Currently, `standalone`, `ray` are supported.
+    """
+
+    def decorator(func):
+        setattr(func, '__test_tags__', tags)
+        return func
+
+    return decorator
 
 
 class DataJuicerTestCaseBase(unittest.TestCase):
@@ -32,3 +51,64 @@ class DataJuicerTestCaseBase(unittest.TestCase):
             if os.path.exists(transformers.TRANSFORMERS_CACHE):
                 print('CLEAN all TRANSFORMERS_CACHE')
                 shutil.rmtree(transformers.TRANSFORMERS_CACHE)
+
+    def generate_dataset(self, data):
+        """Generate dataset for a specific executor.
+
+        Args:
+            type (str, optional): "standalone" or "ray".
+            Defaults to "standalone".
+        """
+        if self.current_tag.startswith('standalone'):
+            return Dataset.from_list(data)
+        elif self.current_tag.startswith('ray'):
+            dataset = rd.from_items(data)
+            if Fields.stats not in dataset.columns(fetch_if_missing=False):
+
+                def process_batch_arrow(table: pa.Table) -> pa.Table:
+                    new_column_data = [{} for _ in range(len(table))]
+                    new_talbe = table.append_column(Fields.stats,
+                                                    [new_column_data])
+                    return new_talbe
+
+                dataset = dataset.map_batches(process_batch_arrow,
+                                              batch_format='pyarrow')
+            return dataset
+        else:
+            raise ValueError('Unsupported type')
+
+    def run_single_op(self, dataset, op, column_names):
+        """Run operator in the specific executor."""
+        if self.current_tag.startswith('standalone'):
+            if isinstance(op, Filter) and Fields.stats not in dataset.features:
+                dataset = dataset.add_column(name=Fields.stats,
+                                             column=[{}] * dataset.num_rows)
+            dataset = dataset.map(op.compute_stats)
+            dataset = dataset.filter(op.process)
+            dataset = dataset.select_columns(column_names=column_names)
+            return dataset.to_list()
+        elif self.current_tag.startswith('ray'):
+            dataset = dataset.map(op.compute_stats)
+            dataset = dataset.filter(op.process)
+            dataset = dataset.to_pandas().get(column_names)
+            if dataset is None:
+                return []
+            return dataset.to_dict(orient='records')
+        else:
+            raise ValueError('Unsupported type')
+
+    def assertDatasetEqual(self, first, second):
+
+        def convert_record(rec):
+            for key in rec.keys():
+                # Convert incomparable `list` to comparable `tuple`
+                if isinstance(rec[key], numpy.ndarray) or isinstance(
+                        rec[key], list):
+                    rec[key] = tuple(rec[key])
+            return rec
+
+        first = [convert_record(d) for d in first]
+        second = [convert_record(d) for d in second]
+        first = sorted(first, key=lambda x: tuple(sorted(x.items())))
+        second = sorted(second, key=lambda x: tuple(sorted(x.items())))
+        return self.assertEqual(first, second)
