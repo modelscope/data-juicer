15d5aa9019cc68fd8732f5314fb2533148c95622
null <3213204+drcege@users.noreply.github.com>
2024-08-20 10:43:12 +0800

    fix checkpoint check

--- data_juicer/utils/ckpt_utils.py
+++ data_juicer/utils/ckpt_utils.py
@@ -87,10 +87,9 @@ class CheckpointManager:
         process_op_num = len(self.process_list)
         if process_op_num < recorded_op_num:
             logger.warning(
-                f'Current config ops ({process_op_num}) are fewer '
-                f'than checkpoint ops ({recorded_op_num}). Cannot '
-                f'reuse checkpoint; All ops will be processed from the '
-                f'beginning')
+                f'Current config ops ({process_op_num}) are fewer than '
+                f'checkpoint ops ({recorded_op_num}). Cannot reuse checkpoint;'
+                f' all ops will be processed from the beginning.')
             self.op_record = []
             return False
 
@@ -113,7 +112,7 @@ class CheckpointManager:
             logger.warning(f'Processed ops of checkpoint are different from '
                            f'current configs: checkpoint-{dif1} vs. config-'
                            f'{dif2}. All ops will be processed from the '
-                           f'beginning')
+                           f'beginning.')
             self.op_record = []
             return False
 
