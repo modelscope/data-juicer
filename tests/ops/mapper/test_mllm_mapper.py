64d81dee74c0fca80d874d1295784378b7ba87f6
Qirui-jiao <luckybanana404@gmail.com>
2024-08-24 10:37:48 +0800

    update

--- tests/ops/mapper/test_mllm_mapper.py
+++ tests/ops/mapper/test_mllm_mapper.py
@@ -10,13 +10,16 @@ class MllmMapperTest(DataJuicerTestCaseBase):
     text_key = 'text'
     image_key = "images"
 
-    def _run_mllm(self):
+    def _run_mllm(self, enable_vllm=False):
         op = MllmMapper(
-            hf_model='llava-v1.6-vicuna-7b-hf'
+            hf_model='llava-v1.6-vicuna-7b-hf',
+            temperature=0.9,
+            top_p=0.95,
+            max_new_tokens=512
         )
 
         samples = [
-            {self.text_key: 'Describe this image.', self.image_key: "./crayon.jpg"},
+            {self.text_key: 'Describe this image.', self.image_key: ["./ipod.jpg", "./crayon.jpg"]},
         ]
 
         for sample in samples:
