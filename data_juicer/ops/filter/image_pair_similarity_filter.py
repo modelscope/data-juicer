ed296a6cd6c13e92b6f7dcaaebe41e2e6a62225b
Qirui-jiao <156628817+Qirui-jiao@users.noreply.github.com>
2024-08-28 17:47:02 +0800

    Update image_pair_similarity_filter.py

--- data_juicer/ops/filter/image_pair_similarity_filter.py
+++ data_juicer/ops/filter/image_pair_similarity_filter.py
@@ -93,12 +93,12 @@ class ImagePairSimilarityFilter(Filter):
         similarity = torch.cosine_similarity(image1_batch_feature,
                                              image2_batch_feature,
                                              dim=1)
-        sample[Fields.stats][StatsKeys.image_text_similarity] = similarity
+        sample[Fields.stats][StatsKeys.image_pair_similarity] = similarity
 
         return sample
 
     def process(self, sample, rank=None):
-        similarity = sample[Fields.stats][StatsKeys.image_text_similarity]
+        similarity = sample[Fields.stats][StatsKeys.image_pair_similarity]
         if len(similarity) <= 0:
             return True
 
