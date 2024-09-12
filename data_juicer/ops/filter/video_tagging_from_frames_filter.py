c40a3086f7d0a3c26092bd60e5a4aca6a011bf2c
Yilun Huang <lielin.hyl@alibaba-inc.com>
2024-09-12 15:28:26 +0800

    Add new OP: image_tagging_mapper (#423)
    * * init image tagging mapper
    
    * + Add unittest for image_tagging_mapper
    * support specified tag field names for all tagging OPs
    
    * * fix problems of unittest
    
    * + add docs
    
    * * update docs
    
    * * skip two unittests which require ram
    
    * * minor fix for gece's comments
    
    * * merge main into this branch
    
    * + add type hint

--- data_juicer/ops/filter/video_tagging_from_frames_filter.py
+++ data_juicer/ops/filter/video_tagging_from_frames_filter.py
@@ -37,6 +37,7 @@ class VideoTaggingFromFramesFilter(Filter):
                  contain: str = 'any',
                  frame_sampling_method: str = 'all_keyframes',
                  frame_num: PositiveInt = 3,
+                 tag_field_name: str = Fields.video_frame_tags,
                  any_or_all: str = 'any',
                  *args,
                  **kwargs):
@@ -61,6 +62,8 @@ class VideoTaggingFromFramesFilter(Filter):
             the first and the last frames will be extracted. If it's larger
             than 2, in addition to the first and the last frames, other frames
             will be extracted uniformly within the video duration.
+        :param tag_field_name: the field name to store the tags. It's
+            "__dj__video_frame_tags__" in default.
         :param any_or_all: keep this sample with 'any' or 'all' strategy of
             all videos. 'any': keep this sample if any videos meet the
             condition. 'all': keep this sample only if all videos meet the
@@ -82,10 +85,12 @@ class VideoTaggingFromFramesFilter(Filter):
         self.tags = set([tag.lower() for tag in tags])
         self.contain_any = (contain == 'any')
         self.any = (any_or_all == 'any')
+        self.tag_field_name = tag_field_name
         self.tagging_producer = VideoTaggingFromFramesMapper(
             frame_sampling_method=frame_sampling_method,
             frame_num=frame_num,
             accelerator=self.accelerator,
+            tag_field_name=self.tag_field_name,
         )
 
     def compute_stats(self, sample, rank=None, context=False):
@@ -95,7 +100,7 @@ class VideoTaggingFromFramesFilter(Filter):
         return sample
 
     def process(self, sample, rank=None):
-        video_tags = sample[Fields.video_frame_tags]
+        video_tags = sample[self.tag_field_name]
         if len(video_tags) <= 0:
             return True
 
