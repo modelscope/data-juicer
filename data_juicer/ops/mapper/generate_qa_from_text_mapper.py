8996389cb285fa7168de410e85e43e1db70fd118
gece.gc <gece.gc@alibaba-inc.com>
2024-10-28 13:16:52 +0000

    unify methods naming

--- data_juicer/ops/mapper/generate_qa_from_text_mapper.py
+++ data_juicer/ops/mapper/generate_qa_from_text_mapper.py
@@ -36,33 +36,23 @@ class GenerateQAFromTextMapper(Mapper):
 
     def __init__(self,
                  hf_model: str = 'alibaba-pai/pai-qwen1_5-7b-doc2qa',
-                 trust_remote_code: bool = False,
-                 pattern: Optional[str] = None,
-                 enable_vllm: bool = True,
-                 tensor_parallel_size: Optional[int] = None,
-                 max_model_len: Optional[int] = None,
-                 max_num_seqs: int = 256,
-                 sampling_params: Dict = {},
-                 *args,
+                 *,
+                 output_pattern: Optional[str] = None,
+                 enable_vllm: bool = False,
+                 llm_params: Optional[Dict] = None,
+                 sampling_params: Optional[Dict] = None,
                  **kwargs):
         """
         Initialization method.
+
         :param hf_model: Hugginface model id.
-        :param trust_remote_code: passed to transformers
-        :param pattern: regular expression pattern to search for within text.
+        :param output_pattern: Regular expression pattern for parsing
+            question and answer from model response.
         :param enable_vllm: Whether to use vllm for inference acceleration.
-        :param tensor_parallel_size: It is only valid when enable_vllm is True.
-            The number of GPUs to use for distributed execution with tensor
-            parallelism.
-        :param max_model_len: It is only valid when enable_vllm is True.
-            Model context length. If unspecified, will be automatically
-            derived from the model config.
-        :param max_num_seqs: It is only valid when enable_vllm is True.
-            Maximum number of sequences to be processed in a single iteration.
+        :param llm_params: LLM initialization parameters.
         :param sampling_params: Sampling parameters for text generation.
             e.g {'temperature': 0.9, 'top_p': 0.95}
-        :param args: extra args
-        :param kwargs: extra args
+        :param kwargs: Extra keyword arguments.
 
         The default data format parsed by this interface is as follows:
         Model Input:
@@ -78,44 +68,40 @@ class GenerateQAFromTextMapper(Mapper):
             ...
         """
 
-        super().__init__(*args, **kwargs)
-        self.num_proc = 1
+        super().__init__(**kwargs)
 
-        if pattern is None:
-            self.pattern = r'Human: (.*?)\nAssistant: (.*?)(?=\nHuman|$)'
+        if output_pattern is None:
+            self.output_pattern = r'Human: (.*?)\nAssistant: (.*?)(?=\nHuman|$)'  # noqa: E501
         else:
-            self.pattern = pattern
+            self.output_pattern = output_pattern
 
         self.enable_vllm = enable_vllm
+        llm_params = llm_params or {}
+        sampling_params = sampling_params or {}
 
         if enable_vllm:
-
             assert torch.cuda.device_count() >= 1, 'must be executed in CUDA'
-            if not tensor_parallel_size:
-                tensor_parallel_size = torch.cuda.device_count()
-                logger.info(f'Set tensor_parallel_size to \
-                    {tensor_parallel_size} for vllm.')
+            if llm_params.get('tensor_parallel_size', 1) > 1:
+                self.num_proc = 1
             self.model_key = prepare_model(
                 model_type='vllm',
                 pretrained_model_name_or_path=hf_model,
-                trust_remote_code=trust_remote_code,
-                tensor_parallel_size=tensor_parallel_size,
-                max_model_len=max_model_len,
-                max_num_seqs=max_num_seqs)
+                **llm_params)
             self.sampling_params = vllm.SamplingParams(**sampling_params)
         else:
             self.model_key = prepare_model(
                 model_type='huggingface',
                 pretrained_model_name_or_path=hf_model,
-                trust_remote_code=trust_remote_code)
+                return_pipe=True,
+                **llm_params)
             self.sampling_params = sampling_params
 
-    def _extract_qa(self, output):
+    def parse_output(self, raw_output):
         """Extract qestion and answer pair from model output response."""
         qa_list = []
 
-        pat = re.compile(self.pattern, re.DOTALL)
-        qa_pairs = pat.findall(output)
+        pat = re.compile(self.output_pattern, re.DOTALL)
+        qa_pairs = pat.findall(raw_output)
 
         for qa in qa_pairs:
             user, assistant = qa
@@ -126,36 +112,34 @@ class GenerateQAFromTextMapper(Mapper):
     def process_batched(self, samples, rank=None):
         model, processor = get_model(self.model_key, rank, self.use_cuda())
 
-        keys = samples.keys()
-        first_key = next(iter(keys))
-        num_samples = len(samples[first_key])
-        out_samples = {
-            key: []
-            for key in keys | {self.query_key, self.response_key}
-        }
+        input_keys = samples.keys()
+        num_samples = len(samples[next(iter(input_keys))])
+        output_keys = input_keys | {self.query_key, self.response_key}
+        output_samples = {key: [] for key in output_keys}
+
         for i in range(num_samples):
-            sample = {key: samples[key][i] for key in keys}
+            messages = [{'role': 'user', 'content': samples[self.text_key][i]}]
+
             if self.enable_vllm:
-                response = model.generate([sample[self.text_key]],
-                                          self.sampling_params)
+                response = model.chat(messages, self.sampling_params)
                 output = response[0].outputs[0].text
             else:
-                inputs = processor(sample[self.text_key],
-                                   return_tensors='pt').to(model.device)
-                response = model.generate(**inputs, **self.sampling_params)
-                output = processor.decode(response.cpu()[0],
-                                          skip_special_tokens=True)
+                # model is pipe
+                response = model(messages,
+                                 return_full_text=False,
+                                 **self.sampling_params)
+                output = response[0]['generated_text']
 
-            qa_list = self._extract_qa(output)
-
+            qa_list = self.parse_output(output)
             if len(qa_list) > 0:
                 for q, a in qa_list:
-                    for k, v in sample.items():
-                        out_samples[k].append(v)
-                    out_samples[self.query_key].append(q)
-                    out_samples[self.response_key].append(a)
+                    for input_k in input_keys:
+                        output_samples[input_k].append(samples[input_k][i])
+                    output_samples[self.query_key].append(q)
+                    output_samples[self.response_key].append(a)
             else:
-                logger.info(
-                    'No question and answer was extracted from this sample!')
+                logger.warning(
+                    'No question and answer was extracted from current sample!'
+                )
 
-        return out_samples
+        return output_samples
