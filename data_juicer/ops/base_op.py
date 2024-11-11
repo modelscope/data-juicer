528b8a9b728e136457f93b93d3b51a9b8de453d3
Ce Ge (戈策) <gece@foxmail.com>
2024-11-11 11:41:56 +0800

    [Ready] Basic Service Implementation (#468)
    * inital implementation
    
    * support params checking and dataset converting
    
    * support filtered and tags
    
    * allow partial execution of op.run
    
    * Service/match api (#428)
    
    * temp
    
    * Regress model preloading (#426)
    
    * fix param definition (#424)
    
    * fix param def
    
    * add param check
    
    * use pydantic types (#422)
    
    * use pydantic types
    
    * change config unittest
    
    * fix GenerateInstructionMapper
    
    * update
    
    * Fix (#427)
    
    Fix some words
    
    * Add new OP: image_tagging_mapper (#423)
    
    * * init image tagging mapper
    
    * + Add unittest for image_tagging_mapper
    * support specified tag field names for all tagging OPs
    
    * * fix problems of unittest
    
    * + add docs
    
    * * update docs
    
    * * skip two unittests which require ram
    
    * * minor fix for gece's comments
    
    * * merge main into this branch
    
    * + add type hint
    
    * match api call
    
    * match api call
    
    * pre-commit
    
    * decouple API args & add type hints
    
    ---------
    
    Co-authored-by: Ce Ge (戈策) <gece@foxmail.com>
    Co-authored-by: Cathy0908 <30484308+Cathy0908@users.noreply.github.com>
    Co-authored-by: co63oc <co63oc@users.noreply.github.com>
    Co-authored-by: Yilun Huang <lielin.hyl@alibaba-inc.com>
    Co-authored-by: null <3213204+drcege@users.noreply.github.com>
    
    * Service/match api (#431)
    
    * temp
    
    * match api call
    
    * match api call
    
    * pre-commit
    
    * decouple API args & add type hints
    
    * agentscope demo
    
    * update demos
    
    * update pre-commt
    
    * add yaml
    
    * update notebooks
    
    * add py
    
    * refine
    
    ---------
    
    Co-authored-by: null <3213204+drcege@users.noreply.github.com>
    Co-authored-by: gece.gc <gece.gc@alibaba-inc.com>
    
    * update reqs
    
    ---------
    
    Co-authored-by: BeachWang <1400012807@pku.edu.cn>
    Co-authored-by: Cathy0908 <30484308+Cathy0908@users.noreply.github.com>
    Co-authored-by: co63oc <co63oc@users.noreply.github.com>
    Co-authored-by: Yilun Huang <lielin.hyl@alibaba-inc.com>

--- data_juicer/ops/base_op.py
+++ data_juicer/ops/base_op.py
@@ -365,7 +365,7 @@ class Filter(OP):
         """
         raise NotImplementedError
 
-    def run(self, dataset, *, exporter=None, tracer=None):
+    def run(self, dataset, *, exporter=None, tracer=None, reduce=True):
         dataset = super(Filter, self).run(dataset)
         if Fields.stats not in dataset.features:
             from data_juicer.core.data import add_same_content_to_new_column
@@ -384,13 +384,16 @@ class Filter(OP):
                               desc=self._name + '_compute_stats')
         if exporter and self.stats_export_path is not None:
             exporter.export_compute_stats(dataset, self.stats_export_path)
-        new_dataset = dataset.filter(self.process,
-                                     num_proc=self.runtime_np(),
-                                     batch_size=self.batch_size,
-                                     desc=self._name + '_process')
-        if tracer:
-            tracer.trace_filter(self._name, dataset, new_dataset)
-        return new_dataset
+        if reduce:
+            new_dataset = dataset.filter(self.process,
+                                         num_proc=self.runtime_np(),
+                                         batch_size=self.batch_size,
+                                         desc=self._name + '_process')
+            if tracer:
+                tracer.trace_filter(self._name, dataset, new_dataset)
+            return new_dataset
+        else:
+            return dataset
 
 
 class Deduplicator(OP):
@@ -436,17 +439,20 @@ class Deduplicator(OP):
         """
         raise NotImplementedError
 
-    def run(self, dataset, *, exporter=None, tracer=None):
+    def run(self, dataset, *, exporter=None, tracer=None, reduce=True):
         dataset = super(Deduplicator, self).run(dataset)
         dataset = dataset.map(self.compute_hash,
                               num_proc=self.runtime_np(),
                               with_rank=self.use_cuda(),
                               desc=self._name + '_compute_hash')
-        show_num = tracer.show_num if tracer else 0
-        new_dataset, dup_pairs = self.process(dataset, show_num)
-        if tracer:
-            tracer.trace_deduplicator(self._name, dup_pairs)
-        return new_dataset
+        if reduce:
+            show_num = tracer.show_num if tracer else 0
+            new_dataset, dup_pairs = self.process(dataset, show_num)
+            if tracer:
+                tracer.trace_deduplicator(self._name, dup_pairs)
+            return new_dataset
+        else:
+            return dataset
 
 
 class Selector(OP):
