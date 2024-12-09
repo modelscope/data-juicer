a365d2109539462b02cd89cae9ec9362cbc61c04
HYLcool <HYLcool@users.noreply.github.com>
2024-12-09 07:35:59 +0000

    deploy: 4b8b436c20b8dc0443bd3c4c3e1c10b17327c04f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.0.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
