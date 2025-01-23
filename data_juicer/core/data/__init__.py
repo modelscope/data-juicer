530efa8df597c624ecb26bff24ca1990813129c6
cyruszhang <cyrus.ylzhang@gmail.com>
2025-01-23 14:28:13 -0800

    add add_same_content_to_new_column reference

--- data_juicer/core/data/__init__.py
+++ data_juicer/core/data/__init__.py
@@ -1,6 +1,9 @@
-from .dj_dataset import DJDataset, NestedDataset, wrap_func_with_nested_access
+from .dj_dataset import (DJDataset, NestedDataset,
+                         add_same_content_to_new_column,
+                         wrap_func_with_nested_access)
 from .ray_dataset import RayDataset
 
 __all__ = [
-    'DJDataset', 'NestedDataset', 'RayDataset', 'wrap_func_with_nested_access'
+    'DJDataset', 'NestedDataset', 'RayDataset', 'wrap_func_with_nested_access',
+    'add_same_content_to_new_column'
 ]
