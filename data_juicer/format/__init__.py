828e7ba01c7fd6cd3d7ef16e2ec526eef31a18c9
cyruszhang <cyrus.ylzhang@gmail.com>
2025-02-07 12:23:25 -0800

    remove mixture formatter; logic captured in dataloader

--- data_juicer/format/__init__.py
+++ data_juicer/format/__init__.py
@@ -5,13 +5,12 @@ from .csv_formatter import CsvFormatter
 from .empty_formatter import EmptyFormatter, RayEmptyFormatter
 from .formatter import LocalFormatter, RemoteFormatter
 from .json_formatter import JsonFormatter
-from .mixture_formatter import MixtureFormatter
 from .parquet_formatter import ParquetFormatter
 from .text_formatter import TextFormatter
 from .tsv_formatter import TsvFormatter
 
 __all__ = [
     'JsonFormatter', 'LocalFormatter', 'RemoteFormatter', 'TextFormatter',
-    'ParquetFormatter', 'CsvFormatter', 'TsvFormatter', 'MixtureFormatter',
-    'EmptyFormatter', 'RayEmptyFormatter'
+    'ParquetFormatter', 'CsvFormatter', 'TsvFormatter', 'EmptyFormatter',
+    'RayEmptyFormatter'
 ]
