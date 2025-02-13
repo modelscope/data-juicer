f73dd418f39a6f6cb14360239fabb248942ea348
cyruszhang <cyrus.ylzhang@gmail.com>
2025-02-13 10:43:15 -0800

    resolve eager RayExecutor importing

--- data_juicer/core/__init__.py
+++ data_juicer/core/__init__.py
@@ -1,8 +1,7 @@
 from .adapter import Adapter
 from .analyzer import Analyzer
 from .data import NestedDataset
-from .executor import Executor, ExecutorFactory, RayExecutor
-from .executor.base import ExecutorBase
+from .executor import Executor
 from .exporter import Exporter
 from .monitor import Monitor
 from .tracer import Tracer
@@ -11,10 +10,7 @@ __all__ = [
     'Adapter',
     'Analyzer',
     'NestedDataset',
-    'ExecutorFactory',
     'Executor',
-    'RayExecutor',
-    'ExecutorBase',
     'Exporter',
     'Monitor',
     'Tracer',
--- data_juicer/core/executor/__init__.py
+++ data_juicer/core/executor/__init__.py
@@ -1,7 +1,5 @@
 from .base import ExecutorBase
 from .default_executor import Executor
 from .factory import ExecutorFactory
-from .ray_executor import RayExecutor
 
-__all__ = ['ExecutorBase'
-           'ExecutorFactory', 'Executor', 'RayExecutor']
+__all__ = ['ExecutorBase', 'ExecutorFactory', 'Executor']
