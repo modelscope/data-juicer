e645b1f159b4148010475d54754c6ad6cfb41b45
Yilun Huang <lielin.hyl@alibaba-inc.com>
2025-02-28 11:30:29 +0800

    Fix logger (#594)
    * * limit the version of huggingface_hub
    * only build docs for py3.10
    
    * - remove SKIPPED_TESTS and use the decoration of `@unittest.skip`
    
    * + Add coverage result to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + save coverage result and add it to artifacts
    
    * + get an initial coverage report
    
    * * only include data_juicer
    
    * * restore ray imports and add ray to minimal requirements
    
    * * fix logger fileno undefined error
    * update downloading logics of models without file locks

--- data_juicer/ops/deduplicator/__init__.py
+++ data_juicer/ops/deduplicator/__init__.py
@@ -1,33 +1,13 @@
-from data_juicer.utils.lazy_loader import LazyLoader
-
 from .document_deduplicator import DocumentDeduplicator
 from .document_minhash_deduplicator import DocumentMinhashDeduplicator
 from .document_simhash_deduplicator import DocumentSimhashDeduplicator
 from .image_deduplicator import ImageDeduplicator
+from .ray_basic_deduplicator import RayBasicDeduplicator
+from .ray_bts_minhash_deduplicator import RayBTSMinhashDeduplicator
+from .ray_document_deduplicator import RayDocumentDeduplicator
+from .ray_image_deduplicator import RayImageDeduplicator
+from .ray_video_deduplicator import RayVideoDeduplicator
 from .video_deduplicator import VideoDeduplicator
-
-RayBasicDeduplicator = LazyLoader(
-    'RayBasicDeduplicator',
-    'data_juicer.ops.deduplicator.ray_basic_deduplicator.RayBasicDeduplicator',
-    auto_install=False)
-RayBTSMinhashDeduplicator = LazyLoader(
-    'RayBTSMinhashDeduplicator',
-    'data_juicer.ops.deduplicator.ray_bts_minhash_deduplicator.'
-    'RayBTSMinhashDeduplicator',
-    auto_install=False)
-RayDocumentDeduplicator = LazyLoader(
-    'RayDocumentDeduplicator',
-    'data_juicer.ops.deduplicator.ray_document_deduplicator.'
-    'RayDocumentDeduplicator',
-    auto_install=False)
-RayImageDeduplicator = LazyLoader(
-    'RayImageDeduplicator',
-    'data_juicer.ops.deduplicator.ray_image_deduplicator.RayImageDeduplicator',
-    auto_install=False)
-RayVideoDeduplicator = LazyLoader(
-    'RayVideoDeduplicator',
-    'data_juicer.ops.deduplicator.ray_video_deduplicator.RayVideoDeduplicator',
-    auto_install=False)
 
 __all__ = [
     'DocumentDeduplicator',
