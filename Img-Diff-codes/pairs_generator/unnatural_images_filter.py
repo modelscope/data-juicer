b39da6ec91b7c66a4687aa3de97eb3028b41d11e
Qirui-jiao <156628817+Qirui-jiao@users.noreply.github.com>
2025-03-16 20:26:19 +0800

    Add files via upload

---
+++ Img-Diff-codes/pairs_generator/unnatural_images_filter.py
@@ -0,0 +1,173 @@
+import math
+import torch
+from transformers import AutoTokenizer, AutoModel
+import numpy as np
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from torchvision.transforms.functional import InterpolationMode
+from transformers import AutoModel, AutoTokenizer
+import os
+import json
+import tqdm
+
+def split_model(model_name):
+    device_map = {}
+    world_size = torch.cuda.device_count()
+    num_layers = {
+        'InternVL2-1B': 24, 'InternVL2-2B': 24, 'InternVL2-4B': 32, 'InternVL2-8B': 32,
+        'InternVL2-26B': 48, 'InternVL2-40B': 60, 'InternVL2-Llama3-76B': 80}[model_name]
+    # Since the first GPU will be used for ViT, treat it as half a GPU.
+    num_layers_per_gpu = math.ceil(num_layers / (world_size - 0.5))
+    num_layers_per_gpu = [num_layers_per_gpu] * world_size
+    num_layers_per_gpu[0] = math.ceil(num_layers_per_gpu[0] * 0.5)
+    layer_cnt = 0
+    for i, num_layer in enumerate(num_layers_per_gpu):
+        for j in range(num_layer):
+            device_map[f'language_model.model.layers.{layer_cnt}'] = i
+            layer_cnt += 1
+    device_map['vision_model'] = 0
+    device_map['mlp1'] = 0
+    device_map['language_model.model.tok_embeddings'] = 0
+    device_map['language_model.model.embed_tokens'] = 0
+    device_map['language_model.output'] = 0
+    device_map['language_model.model.norm'] = 0
+    device_map['language_model.lm_head'] = 0
+    device_map[f'language_model.model.layers.{num_layers - 1}'] = 0
+
+    return device_map
+
+
+IMAGENET_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_STD = (0.229, 0.224, 0.225)
+
+def build_transform(input_size):
+    MEAN, STD = IMAGENET_MEAN, IMAGENET_STD
+    transform = T.Compose([
+        T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
+        T.Resize((input_size, input_size), interpolation=InterpolationMode.BICUBIC),
+        T.ToTensor(),
+        T.Normalize(mean=MEAN, std=STD)
+    ])
+    return transform
+
+def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size):
+    best_ratio_diff = float('inf')
+    best_ratio = (1, 1)
+    area = width * height
+    for ratio in target_ratios:
+        target_aspect_ratio = ratio[0] / ratio[1]
+        ratio_diff = abs(aspect_ratio - target_aspect_ratio)
+        if ratio_diff < best_ratio_diff:
+            best_ratio_diff = ratio_diff
+            best_ratio = ratio
+        elif ratio_diff == best_ratio_diff:
+            if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
+                best_ratio = ratio
+    return best_ratio
+
+def dynamic_preprocess(image, min_num=1, max_num=12, image_size=448, use_thumbnail=False):
+    orig_width, orig_height = image.size
+    aspect_ratio = orig_width / orig_height
+
+    # calculate the existing image aspect ratio
+    target_ratios = set(
+        (i, j) for n in range(min_num, max_num + 1) for i in range(1, n + 1) for j in range(1, n + 1) if
+        i * j <= max_num and i * j >= min_num)
+    target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+
+    # find the closest aspect ratio to the target
+    target_aspect_ratio = find_closest_aspect_ratio(
+        aspect_ratio, target_ratios, orig_width, orig_height, image_size)
+
+    # calculate the target width and height
+    target_width = image_size * target_aspect_ratio[0]
+    target_height = image_size * target_aspect_ratio[1]
+    blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
+
+    # resize the image
+    resized_img = image.resize((target_width, target_height))
+    processed_images = []
+    for i in range(blocks):
+        box = (
+            (i % (target_width // image_size)) * image_size,
+            (i // (target_width // image_size)) * image_size,
+            ((i % (target_width // image_size)) + 1) * image_size,
+            ((i // (target_width // image_size)) + 1) * image_size
+        )
+        # split the image
+        split_img = resized_img.crop(box)
+        processed_images.append(split_img)
+    assert len(processed_images) == blocks
+    if use_thumbnail and len(processed_images) != 1:
+        thumbnail_img = image.resize((image_size, image_size))
+        processed_images.append(thumbnail_img)
+    return processed_images
+
+def load_image(image, input_size=448, max_num=12):
+    transform = build_transform(input_size=input_size)
+    images = dynamic_preprocess(image, image_size=input_size, use_thumbnail=True, max_num=max_num)
+    pixel_values = [transform(image) for image in images]
+    pixel_values = torch.stack(pixel_values)
+    return pixel_values
+
+def concatenate_images_horizontal(images, bar_width):
+    # calc total width of imgs + dist between them
+    total_width = sum(img.width for img in images) + bar_width * (len(images) - 1)
+    # calc max height from imgs
+    height = max(img.height for img in images)
+
+    # create new img with calculated dimensions, black bg
+    new_img = Image.new('RGB', (total_width, height), (0, 0, 0))
+
+    # init var to track current width pos
+    current_width = 0
+    for img in images:
+        # paste img in new_img at current width
+        new_img.paste(img, (current_width, 0))
+        # update current width for next img
+        current_width += img.width + bar_width
+
+    return new_img
+
+if __name__ == "__main__":
+    path = "./model/InternVL2-8B"
+    device_map = split_model('InternVL2-8B')
+    model = AutoModel.from_pretrained(
+        path,
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,
+        use_flash_attn=True,
+        trust_remote_code=True,
+        device_map=device_map).eval()
+
+    tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False)
+
+
+    filter_list = []
+    image_folder = "./image_folder"
+    # set the max number of tiles in `max_num`
+    with open("./img_diff_object_replacement.json", "r") as f:
+        data_file = json.load(f)
+    for json_piece_idx, json_piece in enumerate(tqdm.tqdm(data_file)):
+
+        image1 = Image.open(os.path.join(image_folder, json_piece['path']) + "_0.jpg").convert('RGB')
+        image2 = Image.open(os.path.join(image_folder, json_piece['path']) + "_1.jpg").convert('RGB')
+        image2 = image2.resize((image1.size[0], image1.size[1]))
+        image = concatenate_images_horizontal([image1, image2], 20)
+        pixel_values = load_image(image, max_num=12).to(torch.bfloat16).cuda()
+        generation_config = dict(max_new_tokens=5, do_sample=True)
+
+        question = '<image>\nPlease determine if the content of these two images is reasonable. Answer only with \'yes\' or \'no\'.'
+        response = model.chat(tokenizer, pixel_values, question, generation_config)
+        # print(response)
+        if "no" in response.lower():
+            temp_json = {"path":json_piece['path'], "natural":0}
+        else:
+            temp_json = {"path":json_piece['path'], "natural":1}
+
+        filter_list.append(temp_json)
+        
+    with open("./imgdiff_natural_filter.json", "w") as new_f:
+        new_f.write(json.dumps(filter_list))
+        
