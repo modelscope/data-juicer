7894f51130640957ac55be84dd204f8c3dfd8449
烈霖 <lielin.hyl@alibaba-inc.com>
2025-05-10 22:46:31 +0800

    + Add basic structure for data pool

--- data_juicer/core/sandbox/data_pool_manipulators.py
+++ data_juicer/core/sandbox/data_pool_manipulators.py
@@ -19,10 +19,9 @@ class DataPoolConstruction(BaseDataPoolManipulator):
         Input:
             - an analyzed dataset.
             - an output path.
-            - split_ratios. (optional, [1/3, 2/3] in default)
-            - target number of samples. (optional)
+            - (optional) split_ratios. It's [1/3, 2/3] in default
         Output: MxN data pools, where N is the number of types of analyzed stats and M means the number of split parts.
-            They are named following the rule "<original_name>_<part_idx>_<num_samples>.jsonl"
+            They are named following the rule "<original_name>_<part_idx>.jsonl"
         """
         raise NotImplementedError
 
@@ -32,6 +31,11 @@ class DataPoolCombination(BaseDataPoolManipulator):
     def run(self):
         """
         combine data pool from specified data pools
+
+        Input:
+            - N split data pools, with their ranks.
+        Output: 2^N combined data pools including the original N data pools. Equals to N + C(N, 2) + ... + C(N, N). They
+            are named following the rule "<most_common_prefix>_top_<combined_ranks>.jsonl"
         """
         raise NotImplementedError
 
@@ -41,6 +45,12 @@ class DataPoolDuplication(BaseDataPoolManipulator):
     def run(self):
         """
         duplicate a data pool for specified times
+
+        Input:
+            - N specified data pools.
+            - a list of duplicating times. E.g. [2, 4, 8]
+        Output: NxM new duplicated data pools, where M means the length of the times list. They are named following the
+            rule "<original_name>_x<times>.jsonl"
         """
         raise NotImplementedError
 
@@ -50,6 +60,11 @@ class DataPoolRanking(BaseDataPoolManipulator):
     def run(self):
         """
         rank data pools according to specified evaluation metrics.
+
+        Input:
+            - N specified data pools with their evaluated metrics.
+            - (optional) Some ranking methods or rules. Ranked in descending order in default.
+        Output: A ordered list of data pool paths according to their evaluated metrics.
         """
         raise NotImplementedError
 
@@ -59,5 +74,10 @@ class DataPoolDownsampling(BaseDataPoolManipulator):
     def run(self):
         """
         downsample data pools to specified scale.
+
+        Input:
+            - N specified data pools.
+            - (optional) the target number of samples. It's decided by the smallest data pool in default.
+        Output: N downsampled data pools. They are named following the rule "<original_name>_<num_sample>.jsonl"
         """
         raise NotImplementedError
