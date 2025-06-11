fc6bcb9a84928f1e1e4518b20641820e9cc7d440
chenyushuo <297086016@qq.com>
2025-06-11 19:25:54 +0800

    Add data_format

--- data_juicer/core/data/load_strategy.py
+++ data_juicer/core/data/load_strategy.py
@@ -239,13 +239,47 @@ class RayLocalJsonDataLoadStrategy(RayDataLoadStrategy):
                     f'Current working directory: {os.getcwd()}')
 
         logger.info(f'Using resolved path for loading ray dataset: {path}')
+        data_format = self.cfg.data_format
+        auto_detect = (data_format == 'auto')
+        if auto_detect:
+            item_path = path
+            if os.path.isdir(item_path):
+                path_list = [path]
+                while len(path_list) > 0:
+                    cur_path = path_list.pop()
+                    for item in os.listdir(cur_path):
+                        item_path = os.path.join(cur_path, item)
+                        if os.path.isdir(item_path):
+                            path_list.append(item_path)
+                        elif os.path.isfile(item_path):
+                            break
+            file_extension = os.path.splitext(item_path)[1]
+            file_extension_map = {
+                '.json': 'json',
+                '.jsonl': 'json',
+                '.txt': 'text',
+                '.csv': 'csv',
+                '.tsv': 'csv',
+                '.parquet': 'parquet',
+                '.npy': 'numpy',
+                '.tfrecords': 'tfrecords',
+            }
+            data_format = file_extension_map.get(file_extension, 'json')
+            logger.info(f'Try to load data as {data_format}.')
         try:
-            dataset = RayDataset.read_json(path)
+            dataset = RayDataset.read(data_format, path)
             return RayDataset(dataset, dataset_path=path, cfg=self.cfg)
         except Exception as e:
-            raise RuntimeError(f'Failed to load JSON data from {path}. '
-                               f'Current working directory: {os.getcwd()}. '
-                               f'Error: {str(e)}')
+            if auto_detect:
+                raise RuntimeError(
+                    f'Failed to load data from {path}. Please set the correct `data_format`. '
+                    f'Current working directory: {os.getcwd()}. '
+                    f'Error: {str(e)}')
+            else:
+                raise RuntimeError(
+                    f'Failed to load {data_format} data from {path}. '
+                    f'Current working directory: {os.getcwd()}. '
+                    f'Error: {str(e)}')
 
 
 @DataLoadStrategyRegistry.register('ray', 'remote', 'huggingface')
