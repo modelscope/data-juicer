d81bc560d8ba4c1265588bb5fbacebe1725a3113
cyruszhang <cyrus.ylzhang@gmail.com>
2025-06-23 10:06:45 -0700

    fix pre-commit issue

--- data_juicer/core/sandbox/specific_hooks/text_to_video/preparation_hooks.py
+++ data_juicer/core/sandbox/specific_hooks/text_to_video/preparation_hooks.py
@@ -4,7 +4,9 @@ import os
 from tqdm import tqdm
 
 from data_juicer.core.sandbox.data_pool_manipulators import (
-    BaseDataPoolManipulator, check_io_paths)
+    BaseDataPoolManipulator,
+    check_io_paths,
+)
 from data_juicer.utils.mm_utils import SpecialTokens
 
 
@@ -34,28 +36,24 @@ class DJToEasyAnimateVideoConversion(BaseDataPoolManipulator):
                 "<original_name>_ea_fmt.json"
         """
         # read inputs
-        input_dataset_paths = self.data_pool_cfg.get('dataset_path', [])
-        export_path = self.data_pool_cfg.get('export_path', None)
+        input_dataset_paths = self.data_pool_cfg.get("dataset_path", [])
+        export_path = self.data_pool_cfg.get("export_path", None)
         # check I/O paths
-        existing_input_paths, export_path = check_io_paths(
-            input_dataset_paths, export_path)
+        existing_input_paths, export_path = check_io_paths(input_dataset_paths, export_path)
 
         output_paths = []
-        for src_path in tqdm(
-                existing_input_paths,
-                desc='Converting to EasyAnimate video dataset format'):
+        for src_path in tqdm(existing_input_paths, desc="Converting to EasyAnimate video dataset format"):
             basename = os.path.splitext(os.path.basename(src_path))[0]
-            output_path = os.path.join(export_path, f'{basename}_ea_fmt.json')
-            with open(src_path, 'r') as fin:
+            output_path = os.path.join(export_path, f"{basename}_ea_fmt.json")
+            with open(src_path, "r") as fin:
                 ori_fmt = json.load(fin)
-                with open(output_path, 'w') as fout:
+                with open(output_path, "w") as fout:
                     res_fmt = []
                     for s in ori_fmt:
-                        s['text'] = s['text'].replace(SpecialTokens.video,
-                                                      '').strip()
-                        if len(s['videos']) == 0:
+                        s["text"] = s["text"].replace(SpecialTokens.video, "").strip()
+                        if len(s["videos"]) == 0:
                             continue
-                        s['file_path'] = s['videos'][0]
+                        s["file_path"] = s["videos"][0]
                         res_fmt.append(s)
                     json.dump(res_fmt, fout)
             output_paths.append(output_path)
