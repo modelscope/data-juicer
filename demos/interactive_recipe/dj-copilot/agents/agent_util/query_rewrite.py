d8465655bc1c453fc5817dca2a6acaaab74049da
cmgzn <zdongs@outlook.com>
2025-07-07 15:04:26 +0800

    chore: add licensing information for copied code sections

--- demos/interactive_recipe/dj-copilot/agents/agent_util/query_rewrite.py
+++ demos/interactive_recipe/dj-copilot/agents/agent_util/query_rewrite.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+"""
+Copied from https://github.com/modelscope/agentscope/blob/main/applications/multisource_rag_app/src/agents/agent_util/query_rewrite.py
+
+Data-Juicer adopts Apache 2.0 license, the original license of this file
+is as follows:
+
+Copyright 2024 Alibaba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 # pylint: disable=E0611
 """This module provides a set of query rewrite functions."""
 import time
