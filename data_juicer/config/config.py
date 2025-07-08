94dc2777bd3e76cb32542e36ef6a8c1c4e1d4d09
ricksun2023 <128897743+ricksun2023@users.noreply.github.com>
2025-07-08 11:18:27 +0800

    [bug fix] Fix bug for issue #711: the parameters will be overwritten by the same operator (#730)
    * [bug fix] Fix bug for issue #711: the parameters will be overwritten by the same operators
    
    * [bug fix] Fix bug for issue #711 and UT is passed locally.

--- data_juicer/config/config.py
+++ data_juicer/config/config.py
@@ -778,27 +778,36 @@ def update_op_process(cfg, parser, used_ops=None):
         cfg.process = []
 
     # Create direct mapping of operator names to their configs
-    op_configs = {list(op.keys())[0]: op[list(op.keys())[0]] for op in cfg.process}
+    op_configs = {}
+    for op in cfg.process:
+        op_configs.setdefault(list(op.keys())[0], []).append(op[list(op.keys())[0]])
 
     # Process each used operator
     temp_cfg = cfg
+    op_name_count = {}
     for op_name in used_ops:
         op_config = op_configs.get(op_name)
-
+        op_config_list = []
         if op_name not in option_in_commands:
             # Update op params if set
             if op_config:
-                temp_cfg = parser.merge_config(dict_to_namespace({op_name: op_config}), temp_cfg)
+                for op_c in op_config:
+                    temp_cfg = parser.merge_config(dict_to_namespace({op_name: op_c}), temp_cfg)
+                    oc = namespace_to_dict(temp_cfg)[op_name]
+                    op_config_list.append(oc)
+                temp_cfg = parser.merge_config(dict_to_namespace({op_name: op_config_list}), temp_cfg)
         else:
             # Remove args that will be overridden by command line
             if op_config:
-                for full_option in full_option_in_commands:
-                    key = full_option.split(".")[1]
-                    if key in op_config:
-                        op_config.pop(key)
-
-                if op_config:
-                    temp_cfg = parser.merge_config(dict_to_namespace({op_name: op_config}), temp_cfg)
+                for op_c in op_config:
+                    for full_option in full_option_in_commands:
+                        key = full_option.split(".")[1]
+                        if key in op_c:
+                            op_c.pop(key)
+                    temp_cfg = parser.merge_config(dict_to_namespace({op_name: op_c}), temp_cfg)
+                    oc = namespace_to_dict(temp_cfg)[op_name]
+                    op_config_list.append(oc)
+                temp_cfg = parser.merge_config(dict_to_namespace({op_name: op_config_list}), temp_cfg)
 
         # Update op params
         internal_op_para = temp_cfg.get(op_name)
@@ -806,11 +815,19 @@ def update_op_process(cfg, parser, used_ops=None):
         if op_name in op_configs:
             # Update existing operator
             for i, op_in_process in enumerate(cfg.process):
-                if list(op_in_process.keys())[0] == op_name:
-                    cfg.process[i] = {
-                        op_name: None if internal_op_para is None else namespace_to_dict(internal_op_para)
-                    }
-                    break
+                if isinstance(internal_op_para, list):
+                    if list(op_in_process.keys())[0] == op_name:
+                        if op_name not in op_name_count:
+                            op_name_count[op_name] = 0
+                        else:
+                            op_name_count[op_name] += 1
+                        cfg.process[i] = {
+                            op_name: (
+                                None
+                                if internal_op_para is None
+                                else namespace_to_dict(internal_op_para[op_name_count[op_name]])
+                            )
+                        }
         else:
             # Add new operator
             cfg.process.append({op_name: None if internal_op_para is None else namespace_to_dict(internal_op_para)})
