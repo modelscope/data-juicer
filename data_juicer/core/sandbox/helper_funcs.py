8d8f3d5ca42659d9059556781136e8a69c051b87
烈霖 <lielin.hyl@alibaba-inc.com>
2025-09-12 16:51:55 +0800

    * involve answer of QAs for grader

--- data_juicer/core/sandbox/helper_funcs.py
+++ data_juicer/core/sandbox/helper_funcs.py
@@ -27,3 +27,23 @@ def parse_output(output: str, item: dict, **kwargs):
     A simple implementation.
     """
     return output
+
+
+# Math QA grader
+@ALL_FUNCS.register_module("build_messages_for_math_qa")
+def build_messages_for_math_qa(item: dict, **kwargs):
+    """
+    Build message for math QA grader.
+    """
+    system_key = kwargs.get("system_key", "system")
+    query_key = kwargs.get("query_key", "query")
+    response_key = kwargs.get("response_key", "response")
+
+    system_prompt = item.get(system_key, "")
+    question = item[query_key]
+    answer = item[response_key]
+    messages = []
+    if system_prompt:
+        messages.append({"role": "system", "content": system_prompt})
+    messages.append({"role": "user", "content": f"Question: {question}\nAnswer: {answer}"})
+    return messages
